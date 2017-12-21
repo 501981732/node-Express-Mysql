8140babb7b92352b2231b165deadb29c1929dd86
wangm <501981732@qq.com>
2017-12-21 22:21:28 +0800

    replace jade to html with ejs

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+let ejs = require('ejs')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.engine('.html',ejs.__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
